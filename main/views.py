71829135fa3244e12f58e492ada8a3da65492da7
Moringa School <loisemburu01@gmail.com>
2020-01-06 09:58:30 +0300

    created a macros.html

--- main/views.py
+++ main/views.py
@@ -12,3 +12,10 @@ def index():
     print(popular_news)
     title = 'Home - Welcome to The best News Review Website Online'
     return render_template('index.html', title = title,popular = popular_news)
+
+    # Getting popular movie
+    popular_news = get_news('popular')
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_playing')
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
